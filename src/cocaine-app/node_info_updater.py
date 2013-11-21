807c34693c873903b19dcdd97ddf94329d5e4a5c
Andrey Vasilenkov <nobody.eie@gmail.com>
2013-11-21 18:23:08 +0400

    Async stat requests to nodes

--- src/cocaine-app/node_info_updater.py
+++ src/cocaine-app/node_info_updater.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from contextlib import contextmanager
+import functools
 import json
 import sys
 import threading
@@ -18,9 +19,8 @@ import timed_queue
 import storage
 
 
-GROUP_META_UPDATE_TASK_ID = 'update_symms_for_group_%d'
-COUPLE_META_UPDATE_TASK_ID = 'update_meta_for_couple_%s'
-RESET_HOST_REQUESTS_TASK_ID = 'reset_host_requests'
+GROUPS_META_UPDATE_TASK_ID = 'update_symms_for_groups'
+COUPLES_META_UPDATE_TASK_ID = 'update_meta_for_couples'
 
 
 class NodeInfoUpdater:
@@ -38,10 +38,6 @@ class NodeInfoUpdater:
         self.__session = elliptics.Session(self.__node)
         self.__nodeUpdateTimestamps = (time.time(), time.time())
         self.__cache = Service('cache')
-
-        self._host_tolerable_timeouts = 1
-        self._host_requests = {}
-        self.__host_requests_lock = threading.Lock()
 
         delayed = self.try_restore_from_cache()
         self.loadNodes(delayed=delayed)
@@ -64,38 +60,25 @@ class NodeInfoUpdater:
             raw_stats = self.__session.stat_log()
             storage.update_statistics(raw_stats)
 
-            # need to use keys method to avoid runtime error when
-            # storage.groups changes (when 'delayed' is False)
-            for group in storage.groups.keys():
-                if delayed:
-                    self.__tq.add_task_in(
-                        GROUP_META_UPDATE_TASK_ID % group.group_id,
+            if delayed:
+                self.__tq.add_task_in(
+                        GROUPS_META_UPDATE_TASK_ID,
                         config.get('symm_group_read_gap', 1),
-                        self.updateSymmGroup,
-                        group)
-                else:
-                    self.updateSymmGroup(group)
-
-            for couple in storage.couples:
-                if delayed:
-                    self.__tq.add_task_in(
-                        COUPLE_META_UPDATE_TASK_ID % str(couple),
+                        self.update_symm_groups_async)
+                self.__tq.add_task_in(
+                        COUPLES_META_UPDATE_TASK_ID,
                         config.get('couple_read_gap', 1),
-                        self.updateCoupleMeta,
-                        couple)
-                else:
-                    self.updateCoupleMeta(couple)
-
-            self.__tq.add_task_in(RESET_HOST_REQUESTS_TASK_ID,
-                                  config.get('symm_group_read_gap', 1) + 1,
-                                  self.reset_host_requests)
+                        self.update_couples_meta_async)
+            else:
+                self.update_symm_groups_async()
+                self.update_couples_meta_async()
 
         except Exception as e:
             self.__logging.info('Failed to initialize node updater: %s\n%s' % (str(e), traceback.format_exc()))
             pass
 
     def loadNodes(self, delayed=True):
-        self.__logging.info("Start loading units")
+        self.__logging.info('Start loading units')
         try:
 
             self.execute_tasks(delayed)
@@ -118,88 +101,169 @@ class NodeInfoUpdater:
             self.__nodeUpdateTimestamps = self.__nodeUpdateTimestamps[1:] + (time.time(),)
             bla.setConfigValue("dynamic_too_old_age", max(time.time() - self.__nodeUpdateTimestamps[0], reload_period * 3))
 
-    def updateSymmGroup(self, group):
-        try:
-            self.__logging.info("Trying to read symmetric groups from group %d" % (group.group_id))
+    def update_symm_groups_async(self):
 
-            self.__session.add_groups([group.group_id])
-
-            if not group.nodes:
-                raise errors.OrphanGroupError
-
-            with self.node_request(group.nodes[0]):
-                meta = self.__session.read_data(keys.SYMMETRIC_GROUPS_KEY)
+        _queue = set()
+        def _process_group_metadata(meta, group):
 
             group.parse_meta(meta)
             couples = group.meta['couple']
-            self.__logging.info("Read symmetric groups from group %d: %s" % (group.group_id, couples))
+            self.__logging.info('Read symmetric groups from group '
+                '{0}: {1}'.format(group.group_id, couples))
             for group_id2 in couples:
                 if group_id2 != group.group_id:
-                    self.__logging.info("Scheduling update for group %s" % group_id2)
-                    self.__tq.hurry(GROUP_META_UPDATE_TASK_ID % group_id2)
+                    self.__logging.info('Scheduling update '
+                        'for group {0}'.format(group_id2))
+                    _queue.add(group_id2)
 
             couple_str = ':'.join((str(g) for g in sorted(couples)))
-            self.__logging.debug('%s in storage.couples: %s' % (couple_str, couple_str in storage.couples))
-            self.__logging.debug('Keys in storage.couples: %s' % [str(c) for c in storage.couples])
+
+            self.__logging.debug('{0} in storage.couples: {1}'.format(
+                couple_str, couple_str in storage.couples))
+            self.__logging.debug('Keys in storage.couples: {0}'.format(
+                [str(c) for c in storage.couples]))
 
             if not couple_str in storage.couples:
-                self.__logging.info("Creating couple %s" % (couple_str))
+                self.__logging.info('Creating couple {0}'.format(couple_str))
                 for gid in couples:
                     if not gid in storage.groups:
-                        self.__logging.info("Group %s doesn't exist in all_groups, add fake data with couples=%s" % (gid, couples))
+                        self.__logging.info("Group {0} doesn't exist in "
+                            "all_groups, add fake data with couples={1}".format(gid, couples))
                         storage.groups.add(gid)
                 c = storage.couples.add([storage.groups[gid] for gid in couples])
-                self.__logging.info("Created couple %s %s" % (c, repr(c)))
-                self.__tq.add_task_in(COUPLE_META_UPDATE_TASK_ID % str(c),
-                                      config.get('couple_read_gap', 1),
-                                      self.updateCoupleMeta,
-                                      c)
-            else:
-                self.__logging.debug("Couple %s already exists" % couple_str)
+                self.__logging.info('Created couple {0} {1}'.format(c, repr(c)))
+            return
 
-            storage.couples[couple_str].update_status()
-        except errors.OrphanGroupError:
-            self.__logging.info('Skipping group %d, nodes data is unavailable' % group.group_id)
-        except errors.SkipHostError:
-            pass
-        except Exception as e:
-            self.__logging.error("Failed to read symmetric_groups from group %d (%s), %s" % (group.group_id, str(e), traceback.format_exc()))
-            group.parse_meta(None)
-        except:
-            self.__logging.error("Failed2 to read symmetric_groups from group %d (%s), %s" % (group.group_id, sys.exc_info()[0], traceback.format_exc()))
-            group.parse_meta(None)
-
-        finally:
-            if group.couple:
-                group.couple.update_status()
-            else:
-                group.update_status()
-
-    def updateCoupleMeta(self, couple):
         try:
-            self.__logging.info('Reading couple %s meta data' % couple)
-            data = None
+            requests = []
+
+            def _session_setup(session, groups):
+                session.add_groups(groups)
+                return session
+
+            groups = storage.groups.keys()
+
+            for group in groups:
+                group_id = group.group_id
+                requests.append((functools.partial(_session_setup, self.__session, [group_id]),
+                                 group_id, keys.SYMMETRIC_GROUPS_KEY))
+            results = self._parallel_read(requests)
+            [group.parse_meta(None) for group in groups if not group.group_id in results]
+
+            while results:
+                if _queue:
+                    group_id = _queue.pop()
+                    if group_id not in results:
+                        continue
+                    result = results.pop(group_id)
+                else:
+                    group_id, result = results.popitem()
+
+                group = storage.groups[group_id]
+
+                try:
+                    self.__logging.debug('Reading symmetric groups '
+                        'from group {0}'.format(group.group_id))
+                    self._process_elliptics_response(_process_group_metadata,
+                                                     result, group)
+                except ValueError as e:
+                    self.__logging.warn('Failed to read symmetric_groups '
+                        'from group {0}: {1}'.format(group_id, e))
+                    group.parse_meta(None)
+                except Exception as e:
+                    self.__logging.error('Failed to read symmetric_groups '
+                        'from group {0}: {1}, {2}'.format(
+                            group_id, e, traceback.format_exc()))
+                    group.parse_meta(None)
+                finally:
+                    if group.couple:
+                        group.couple.update_status()
+                    else:
+                        group.update_status()
+
+        except Exception as e:
+            self.__logging.error('Critical error during symmetric group '
+                                 'update, {0}: {1}'.format(str(e),
+                                     traceback.format_exc()))
+
+    def update_couples_meta_async(self):
+        try:
+            requests = []
+
+            couples = storage.couples.keys()
+
+            for couple in couples:
+                requests.append((self.__node.meta_session, couple,
+                                 keys.MASTERMIND_COUPLE_META_KEY % str(couple)))
+            results = self._parallel_read(requests)
+            [couple.parse_meta(None) for couple in couples if not couple in results]
+
+            while results:
+                couple, result = results.popitem()
+                try:
+                    self.__logging.debug('Reading couple {0} metadata'.format(
+                        str(couple)))
+                    self._process_elliptics_response(couple.parse_meta, result)
+                    self.__logging.info('Updated couple metadata (frozen) '
+                        'for couple {0}'.format(str(couple)))
+                except ValueError as e:
+                    self.__logging.debug('Failed to read couple {0} metadata: '
+                        '{1}'.format(couple, e))
+                    couple.parse_meta(None)
+                except Exception as e:
+                    self.__logging.error('Failed to read couple {0} metadata: '
+                        '{1}, {2}'.format(couple, e, traceback.format_exc()))
+                    couple.parse_meta(None)
+                finally:
+                    couple.update_status()
+
+        except Exception as e:
+            self.__logging.error('Critical error during couples metadata '
+                                 'update, {0}: {1}'.format(str(e),
+                                     traceback.format_exc()))
+
+    def _parallel_read(self, requests):
+        results = {}
+        for session, result_key, key in requests:
+            if callable(session):
+                session = session()
+
+            # read_latest_async is for elliptics 2.24.14.15, in later versions
+            # read_data returns AsyncResult object
+            self.__logging.debug('Request to read {0} for groups {1}'.format(
+                                 key.replace('\0', '\\0'), result_key))
+            read = getattr(session, 'read_latest_async', session.read_data)
+
             try:
-                data = self.__node.meta_session.read_latest(keys.MASTERMIND_COUPLE_META_KEY % str(couple))
-            except elliptics.NotFoundError:
-                self.__logging.debug('No metadata (frozen) found '
-                                     'for couple %s' % couple)
-                couple.parse_meta(None)
-                couple.update_status()
-                return
-            couple.parse_meta(data)
-            self.__logging.info('Parsed metadata for couple %s' % couple)
-        except BaseException as e:
-            self.__logging.error('Failed to read meta for couple %s (%s), '
-                                 '%s' % (couple, str(e),
-                                         traceback.format_exc()))
-            couple.parse_meta(None)
+                results[result_key] = read(elliptics.Id(key))
+            except Exception as e:
+                self.__logging.error('Failed to read {0} for groups '
+                    '{1}: {2}, {3}'.format(key, result_key,
+                                           str(e), traceback.format_exc()))
+                pass
 
-        couple.update_status()
+        return results
 
-    def reset_host_requests(self):
-        with self.__host_requests_lock:
-            self._host_requests = {}
+    @staticmethod
+    def _process_elliptics_response(processor, result, *args):
+        # get result if it is wrapped in AsyncResult
+        # TODO: change check to isinstance(result, elliptics.AsyncResult) when fixed
+        if getattr(result, 'get'):
+            try:
+                result.wait()
+            except Exception as e:
+                raise ValueError(e)
+            if not result.successful():
+                raise ValueError('key not found')
+            if not len(result.get()):
+                raise ValueError('empty response')
+            entry = result.get()[0]
+            if getattr(entry, 'error', None) and entry.error.code:
+                raise ValueError('result code {0}, msg {1}'.format(
+                    entry.error.code, entry.error.message))
+            result = str(entry.data)
+
+        return processor(result, *args)
 
     def restore_state(self, state):
         state = json.loads(state)
@@ -252,34 +316,6 @@ class NodeInfoUpdater:
         })
         self.__logging.info('Saving cached state')
         self.__cache.put(self.STORAGE_STATE_CACHE_KEY, state)
-
-    @contextmanager
-    def node_request(self, node):
-        with self.__host_requests_lock:
-            host_requests = self._host_requests.setdefault(node.host,
-                {'skip': 0, 'timeouts': 0})
-            self.__logging.info('skip counter: %s' % (host_requests,))
-            skip = host_requests['skip']
-            if skip > 0:
-                self.__logging.info('Skipping request to host {0}, '
-                    'skip counter for host: {1}'.format(
-                        node.host, skip))
-                host_requests['skip'] = skip - 1
-                raise errors.SkipHostError
-
-        try:
-            yield
-        except elliptics.TimeoutError:
-            with self.__host_requests_lock:
-                host_requests = self._host_requests[node.host]
-                host_requests['timeouts'] += 1
-                if host_requests['timeouts'] > self._host_tolerable_timeouts:
-                    timeouts = host_requests['timeouts'] - self._host_tolerable_timeouts
-                    skip = 2 ** (timeouts - 1)
-                    host_requests['skip'] = skip
-                    self.__logging.info('Setting skip counter '
-                        'for host {0}: {1}'.format(node.host, skip))
-                raise
 
     def stop(self):
         self.__tq.shutdown()
