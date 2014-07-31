3f0d5f4f9a4ae30cda094b3b997a0e3fa61ba836
Andrey Vasilenkov <nobody.eie@gmail.com>
2014-07-31 18:37:57 +0400

    Minor kazoo changes

--- src/cocaine-app/sync/kazoo_impl/lock.py
+++ src/cocaine-app/sync/kazoo_impl/lock.py
@@ -30,7 +30,7 @@ class Lock(object):
 
     Example usage with a :class:`~kazoo.client.KazooClient` instance:
 
-    .. code-block:: python 
+    .. code-block:: python
 
         zk = KazooClient()
         lock = zk.Lock("/lockpath", "my-identifier")
@@ -104,13 +104,13 @@ class Lock(object):
             retry.deadline = timeout
             self.is_acquired = retry(self._inner_acquire,
                 blocking=blocking, timeout=timeout)
+        except RetryFailedError:
+            self._best_effort_cleanup()
         except KazooException:
             # if we did ultimately fail, attempt to clean up
             self._best_effort_cleanup()
             self.cancelled = False
             raise
-        except RetryFailedError:
-            self._best_effort_cleanup()
 
         if not self.is_acquired:
             self._delete_node(self.node)
@@ -241,262 +241,6 @@ class Lock(object):
             except NoNodeError:  # pragma: nocover
                 pass
         return contenders
-
-    def __enter__(self):
-        self.acquire()
-
-    def __exit__(self, exc_type, exc_value, traceback):
-        self.release()
-
-
-class Semaphore(object):
-    """A Zookeeper-based Semaphore
-
-    This synchronization primitive operates in the same manner as the
-    Python threading version only uses the concept of leases to
-    indicate how many available leases are available for the lock
-    rather than counting.
-
-    Example:
-
-    .. code-block:: python
-
-        zk = KazooClient()
-        semaphore = zk.Semaphore("/leasepath", "my-identifier")
-        with semaphore:  # blocks waiting for lock acquisition
-            # do something with the semaphore
-
-    .. warning::
-
-        This class stores the allowed max_leases as the data on the
-        top-level semaphore node. The stored value is checked once
-        against the max_leases of each instance. This check is
-        performed when acquire is called the first time. The semaphore
-        node needs to be deleted to change the allowed leases.
-
-    .. versionadded:: 0.6
-        The Semaphore class.
-
-    .. versionadded:: 1.1
-        The max_leases check.
-
-    """
-    def __init__(self, client, path, identifier=None, max_leases=1):
-        """Create a Kazoo Lock
-
-        :param client: A :class:`~kazoo.client.KazooClient` instance.
-        :param path: The semaphore path to use.
-        :param identifier: Name to use for this lock contender. This
-                           can be useful for querying to see who the
-                           current lock contenders are.
-        :param max_leases: The maximum amount of leases available for
-                           the semaphore.
-
-        """
-        # Implementation notes about how excessive thundering herd
-        # and watches are avoided
-        # - A node (lease pool) holds children for each lease in use
-        # - A lock is acquired for a process attempting to acquire a
-        #   lease. If a lease is available, the ephemeral node is
-        #   created in the lease pool and the lock is released.
-        # - Only the lock holder watches for children changes in the
-        #   lease pool
-        self.client = client
-        self.path = path
-
-        # some data is written to the node. this can be queried via
-        # contenders() to see who is contending for the lock
-        self.data = str(identifier or "").encode('utf-8')
-        self.max_leases = max_leases
-        self.wake_event = client.handler.event_object()
-
-        self.create_path = self.path + "/" + uuid.uuid4().hex
-        self.lock_path = path + '-' + '__lock__'
-        self.is_acquired = False
-        self.assured_path = False
-        self.cancelled = False
-        self._session_expired = False
-
-    def _ensure_path(self):
-        result = self.client.ensure_path(self.path)
-        self.assured_path = True
-        if result is True:
-            # node did already exist
-            data, _ = self.client.get(self.path)
-            try:
-                leases = int(data.decode('utf-8'))
-            except (ValueError, TypeError):
-                # ignore non-numeric data, maybe the node data is used
-                # for other purposes
-                pass
-            else:
-                if leases != self.max_leases:
-                    raise ValueError(
-                        "Inconsistent max leases: %s, expected: %s" %
-                        (leases, self.max_leases)
-                    )
-        else:
-            self.client.set(self.path, str(self.max_leases).encode('utf-8'))
-
-    def cancel(self):
-        """Cancel a pending semaphore acquire."""
-        self.cancelled = True
-        self.wake_event.set()
-
-    def acquire(self, blocking=True, timeout=None):
-        """Acquire the semaphore. By defaults blocks and waits forever.
-
-        :param blocking: Block until semaphore is obtained or
-                         return immediately.
-        :type blocking: bool
-        :param timeout: Don't wait forever to acquire the semaphore.
-        :type timeout: float or None
-
-        :returns: Was the semaphore acquired?
-        :rtype: bool
-
-        :raises:
-            ValueError if the max_leases value doesn't match the
-            stored value.
-
-            :exc:`~kazoo.exceptions.LockTimeout` if the semaphore
-            wasn't acquired within `timeout` seconds.
-
-        .. versionadded:: 1.1
-            The blocking, timeout arguments and the max_leases check.
-        """
-        # If the semaphore had previously been canceled, make sure to
-        # reset that state.
-        self.cancelled = False
-
-        try:
-            self.is_acquired = self.client.retry(
-                self._inner_acquire, blocking=blocking, timeout=timeout)
-        except KazooException:
-            # if we did ultimately fail, attempt to clean up
-            self._best_effort_cleanup()
-            self.cancelled = False
-            raise
-
-        return self.is_acquired
-
-    def _inner_acquire(self, blocking, timeout=None):
-        """Inner loop that runs from the top anytime a command hits a
-        retryable Zookeeper exception."""
-        self._session_expired = False
-        self.client.add_listener(self._watch_session)
-
-        if not self.assured_path:
-            self._ensure_path()
-
-        # Do we already have a lease?
-        if self.client.exists(self.create_path):
-            return True
-
-        with self.client.Lock(self.lock_path, self.data):
-            while True:
-                self.wake_event.clear()
-
-                # Attempt to grab our lease...
-                if self._get_lease():
-                    return True
-
-                if blocking:
-                    # If blocking, wait until self._watch_lease_change() is
-                    # called before returning
-                    self.wake_event.wait(timeout)
-                    if not self.wake_event.isSet():
-                        raise LockTimeout(
-                            "Failed to acquire semaphore on %s "
-                            "after %s seconds" % (self.path, timeout))
-                else:
-                    # If not blocking, register another watch that will trigger
-                    # self._get_lease() as soon as the children change again.
-                    self.client.get_children(self.path, self._get_lease)
-                    return False
-
-    def _watch_lease_change(self, event):
-        self.wake_event.set()
-
-    def _get_lease(self, data=None):
-        # Make sure the session is still valid
-        if self._session_expired:
-            raise ForceRetryError("Retry on session loss at top")
-
-        # Make sure that the request hasn't been canceled
-        if self.cancelled:
-            raise CancelledError("Semaphore cancelled")
-
-        # Get a list of the current potential lock holders. If they change,
-        # notify our wake_event object. This is used to unblock a blocking
-        # self._inner_acquire call.
-        children = self.client.get_children(self.path,
-                                            self._watch_lease_change)
-
-        # If there are leases available, acquire one
-        if len(children) < self.max_leases:
-            self.client.create(self.create_path, self.data, ephemeral=True)
-
-        # Check if our acquisition was successful or not. Update our state.
-        if self.client.exists(self.create_path):
-            self.is_acquired = True
-        else:
-            self.is_acquired = False
-
-        # Return current state
-        return self.is_acquired
-
-    def _watch_session(self, state):
-        if state == KazooState.LOST:
-            self._session_expired = True
-            self.wake_event.set()
-
-            # Return true to de-register
-            return True
-
-    def _best_effort_cleanup(self):
-        try:
-            self.client.delete(self.create_path)
-        except KazooException:  # pragma: nocover
-            pass
-
-    def release(self):
-        """Release the lease immediately."""
-        return self.client.retry(self._inner_release)
-
-    def _inner_release(self):
-        if not self.is_acquired:
-            return False
-
-        try:
-            self.client.delete(self.create_path)
-        except NoNodeError:  # pragma: nocover
-            pass
-        self.is_acquired = False
-        return True
-
-    def lease_holders(self):
-        """Return an unordered list of the current lease holders.
-
-        .. note::
-
-            If the lease holder did not set an identifier, it will
-            appear as a blank string.
-
-        """
-        if not self.client.exists(self.path):
-            return []
-
-        children = self.client.get_children(self.path)
-
-        lease_holders = []
-        for child in children:
-            try:
-                data, stat = self.client.get(self.path + "/" + child)
-                lease_holders.append(data.decode('utf-8'))
-            except NoNodeError:  # pragma: nocover
-                pass
-        return lease_holders
 
     def __enter__(self):
         self.acquire()
