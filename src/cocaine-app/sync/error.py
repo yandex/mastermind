61532737af661c4b98cf2d640502ce07ab7215f8
Andrey Vasilenkov <nobody.eie@gmail.com>
2014-09-16 19:32:13 +0400

    Error module drafts

---
+++ src/cocaine-app/sync/error.py
@@ -0,0 +1,54 @@
+
+
+class API_ERROR_CODE(object):
+
+    LOCK = 20
+    LOCK_FAILED = 21
+    LOCK_ALREADY_ACQUIRED = 22
+    LOCK_INCONSISTENT = 23
+
+
+class LockError(Exception):
+
+    PARAMS = ()
+
+    def __init__(self, *args, **kwargs):
+        super(LockError, self).__init__(*args)
+        for param in self.PARAMS:
+            setattr(self, param, kwargs.get(param, None))
+
+    @property
+    def code(self):
+        return API_ERROR_CODE.LOCK
+
+    def dump(self):
+        res = {
+            'msg': str(self),
+            'code': self.code
+        }
+        for param in self.PARAMS:
+            res[param] = getattr(self, param)
+        return res
+
+class LockFailedError(LockError):
+
+    PARAMS = ('lock_id',)
+
+    @property
+    def code(self):
+        return API_ERROR_CODE.LOCK_FAILED
+
+
+class LockAlreadyAcquiredError(LockFailedError):
+
+    PARAMS = ('lock_id', 'holder_id',)
+
+    @property
+    def code(self):
+        return API_ERROR_CODE.LOCK_ALREADY_ACQUIRED
+
+
+class InconsistentLockError(LockAlreadyAcquiredError):
+    @property
+    def code(self):
+        return API_ERROR_CODE.LOCK_INCONSISTENT
