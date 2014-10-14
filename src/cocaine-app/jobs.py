f84a159f08dff90bc13055be6de995457b2c2dcd
Andrey Vasilenkov <nobody.eie@gmail.com>
2014-10-14 15:39:47 +0400

    Move jobs: check src couple status before stopping node backnend

--- src/cocaine-app/jobs.py
+++ src/cocaine-app/jobs.py
@@ -650,26 +650,33 @@ class NodeStopTask(MinionCmdTask):
                 'consistency'.format(self.parent_job.id, self.id, self.group, self.host))
 
             if not self.group in storage.groups:
-                raise JobBrokenError('Group {0} is not found'.format(self.group))
+                raise JobBrokenError('Task {0}: group {0} is not found'.format(self.id, self.group))
 
             group = storage.groups[self.group]
             if len(group.node_backends) != 1 or group.node_backends[0].node.host.addr != self.host:
                 raise JobBrokenError('Task {0}: group {1} has more than '
-                    'one node backend: {2}, expected host {3}'.format(self, self.group,
+                    'one node backend: {2}, expected host {3}'.format(self.id, self.group,
                         [str(nb) for nb in group.node_backends], self.host))
 
             if group.node_backends[0].status != storage.Status.OK:
                 raise JobBrokenError('Task {0}: node of group {1} has '
-                    'status {2}, should be {3}'.format(self, self.group,
+                    'status {2}, should be {3}'.format(self.id, self.group,
                         self.node_backends[0].status, storage.Status.OK))
 
             if self.uncoupled:
                 if group.couple:
                     raise JobBrokenError('Task {0}: group {1} happens to be '
-                        'already coupled'.format(self, self.group))
+                        'already coupled'.format(self.id, self.group))
                 if group.node_backends[0].stat.files + group.node_backends[0].stat.files_removed > 0:
                     raise JobBrokenError('Task {0}: group {1} has non-zero '
-                        'number of keys (including removed)')
+                        'number of keys (including removed)'.format(self.id, self.group))
+            else:
+                if not group.couple:
+                    raise JobBrokenError('Task {0}: group {1} is not '
+                        'coupled'.format(self.id, self.group))
+                if group.couple.status not in storage.GOOD_STATUSES:
+                    raise JobBrokenError('Task {0}: group {1} couple {2} '
+                        'status is {3}'.format(self.id, self.group, str(group.couple), group.couple.status))
 
         super(NodeStopTask, self).execute(minions)
 
