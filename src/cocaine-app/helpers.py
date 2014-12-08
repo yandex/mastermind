b8dc7a56ce4ad5c3414106979f30b9fbf320595e
Andrey Vasilenkov <nobody.eie@gmail.com>
2014-12-08 17:09:51 +0300

    Minor refactoring

--- src/cocaine-app/helpers.py
+++ src/cocaine-app/helpers.py
@@ -2,6 +2,8 @@ from functools import wraps
 import logging
 import traceback
 
+import elliptics
+
 
 logger = logging.getLogger('mm.balancer')
 
@@ -16,3 +18,38 @@ def handler(f):
             return {'Error': str(e)}
 
     return wrapper
+
+
+def session_op_retry(op, acc_codes):
+
+    def wrapped_retry(session, *args, **kwargs):
+        """
+        Returns tuple of (successful groups, failed_groups)
+        """
+        s = session.clone()
+
+        groups = set(s.groups)
+        dest_groups = set(s.groups)
+
+        s.set_checker(elliptics.checkers.no_check)
+        s.set_exceptions_policy(elliptics.exceptions_policy.no_exceptions)
+        s.set_filter(elliptics.filters.all_with_ack)
+
+        retries = kwargs.pop('retries', 3)
+
+        for i in xrange(retries):
+            s.set_groups(dest_groups)
+            res = getattr(s, op)(*args).get()
+            success_groups = set(
+                [r.group_id for r in res if r.error.code in acc_codes])
+            dest_groups -= success_groups
+            if not dest_groups:
+                break
+
+        return groups - dest_groups, dest_groups
+
+    return wrapped_retry
+
+
+write_retry = session_op_retry('write_data', (0,))
+remove_retry = session_op_retry('remove', (0, -2))
