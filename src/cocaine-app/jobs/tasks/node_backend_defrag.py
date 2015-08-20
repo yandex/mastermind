0591a4d1782d8cc50816b7eeec5da13912827545
Andrey Vasilenkov <indigo@yandex-team.ru>
2015-08-20 12:36:54 +0300

    Fix for node backend defrag task retrying

--- src/cocaine-app/jobs/tasks/node_backend_defrag.py
+++ src/cocaine-app/jobs/tasks/node_backend_defrag.py
@@ -19,11 +19,12 @@ class NodeBackendDefragTask(MinionCmdTask):
     def execute(self, processor):
         # checking if task still applicable
         logger.info('Job {0}, task {1}: checking group {2} and node backend {3} '
-            'consistency'.format(self.parent_job.id, self.id, self.group, self.node_backend))
+                    'consistency'.format(
+                        self.parent_job.id, self.id, self.group, self.node_backend))
 
-        if not self.group in storage.groups:
+        if self.group not in storage.groups:
             raise JobBrokenError('Group {0} is not found'.format(self.group))
-        if not self.node_backend in storage.node_backends:
+        if self.node_backend not in storage.node_backends:
             raise JobBrokenError('Node backend {0} is not found'.format(self.node_backend))
 
         group = storage.groups[self.group]
@@ -31,14 +32,14 @@ class NodeBackendDefragTask(MinionCmdTask):
 
         if group.couple is None:
             raise JobBrokenError('Task {0}: group {1} does not belong '
-                'to any couple'.format(self, self.group))
+                                 'to any couple'.format(self, self.group))
 
         if group.couple.status not in storage.GOOD_STATUSES:
-            raise RetryError('Task {0}: group {1} couple status is {2}'.format(
-                self, self.group, group.couple.status))
+            raise RetryError(10, JobBrokenError('Task {}: group {} couple status is {}'.format(
+                self, self.group, group.couple.status)))
 
         if node_backend not in group.node_backends:
             raise JobBrokenError('Task {0}: node backend {1} does not belong to '
-                'group {2}'.format(self, self.node_backend, self.group))
+                                 'group {2}'.format(self, self.node_backend, self.group))
 
         super(NodeBackendDefragTask, self).execute(processor)
