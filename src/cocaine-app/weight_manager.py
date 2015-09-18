d1a1e44301ccdb4b0ff20e73a7e5a6ff1f9265ef
Andrey Vasilenkov <indigo@yandex-team.ru>
2015-09-18 12:57:52 +0300

    Weight manager updated

--- src/cocaine-app/weight_manager.py
+++ src/cocaine-app/weight_manager.py
@@ -10,11 +10,13 @@ import logging
 
 logger = logging.getLogger('mm.weights')
 
+WEIGHT_CFG = config.get('weight', {})
+
 
 class WeightManager(object):
 
-    MIN_NS_UNITS = config.get('weight', {}).get('min_units', 1)
-    ADD_NS_UNITS = config.get('weight', {}).get('add_units', 1)
+    MIN_NS_UNITS = WEIGHT_CFG.get('min_units', 1)
+    ADD_NS_UNITS = WEIGHT_CFG.get('add_units', 1)
 
     def __init__(self):
         self.disks = {}
@@ -42,11 +44,15 @@ class WeightManager(object):
                 nbs_res = []
                 for nb in group.node_backends:
                     nb_hostname = nb.node.host.hostname
-                    net_res = net.setdefault(nb_hostname,
-                                             NetResources(load_manager.net[nb_hostname]))
+                    net_res = net.setdefault(
+                        nb_hostname,
+                        NetResources(nb_hostname, load_manager.net[nb_hostname])
+                    )
                     disk_key = (nb_hostname, nb.fs.fsid)
-                    disk_res = disks.setdefault(disk_key,
-                                                DiskResources(load_manager.disks[disk_key]))
+                    disk_res = disks.setdefault(
+                        disk_key,
+                        DiskResources(disk_key, load_manager.disks[disk_key])
+                    )
                     nbs_res.append(
                         NodeBackendResources(
                             disk_res,
@@ -69,6 +75,8 @@ class WeightManager(object):
     def calculate_weights(self, storage):
         try:
             weights = {}
+            min_couple_res_units = WeightCalculator.min_couple_resources()
+            zero_res_units = WeightCalculator.zero_resources()
             for ns in self.namespaces(storage):
                 ns_weights = {}
                 ns_sizes = {}
@@ -95,6 +103,7 @@ class WeightManager(object):
                     claimed_units = WeightCalculator.zero_resources()
 
                     ns_weights[ns_size] = []
+                    skip_couples = set()
 
                     for couple_res in buckets:
                         logger.debug('Ns {}, calculating weight for couple {}'.format(
@@ -103,25 +112,32 @@ class WeightManager(object):
                         ))
                         # count couple weight, accumulate it
                         weight, couple_res_units = WeightCalculator.calculate_resources(couple_res)
-                        claimed_units += couple_res_units
+                        # change the state of resources
+                        if required_units - claimed_units > zero_res_units:
+                            claim_res_units = min(
+                                couple_res_units,
+                                required_units - claimed_units
+                            )
+                        else:
+                            claim_res_units = min_couple_res_units
+                        self.__claim(couple_res, claim_res_units)
+                        # mark couples with shared resources
+                        claimed_units += claim_res_units
                         logger.debug('Ns {}, acc claimed resources: {}'.format(
                             ns.id,
                             claimed_units
                         ))
-                        # change the state of resources
-                        self.__claim(couple_res, couple_res_units)
-                        # mark couples with shared resources
-                        self.__rebucket(buckets, couple_res)
+                        skip_couples.add(couple_res.couple)
+                        self.__rebucket(buckets, couple_res, skip_couples, ns_size)
                         ns_weights[ns_size].append((
                             couple_res.couple.as_tuple(),
                             weight,
                             couple_res.couple.effective_free_space
                         ))
-                        # TODO: commented for tests
-                        # enough_couples = len(ns_weights[ns_size]) >= ns_min_units + ns_add_units
-                        # if claimed_units >= required_units and enough_couples:
-                        #     # claimed enough resouce units for namespace
-                        #     break
+                        enough_couples = len(ns_weights[ns_size]) >= ns_min_units + ns_add_units
+                        if claimed_units >= required_units and enough_couples:
+                            # claimed enough resouce units for namespace
+                            break
 
                 ns_groups_count = infrastructure.ns_settings[ns]['groups-count']
                 found_couples = len(ns_weights.get(ns_groups_count, []))
@@ -134,7 +150,6 @@ class WeightManager(object):
                             ns_min_units
                         )
                     )
-                    # TODO: log error message
                     continue
                 else:
                     weights[ns.id] = ns_weights
@@ -144,22 +159,25 @@ class WeightManager(object):
             logger.exception('Failed to calculate weights')
             pass
 
-    def __rebucket(self, buckets, couple_res):
+    def __rebucket(self, buckets, couple_res, skip_couples, couple_size):
         disk_keys = couple_res.disks_keys()
         net_keys = couple_res.net_keys()
 
         couples_to_rebucket = set()
-        # TODO: code duplication!
-        for disk_key in disk_keys:
-            for couple in self.couples_by_disk.get(disk_key):
-                if couple.namespace != couple_res.couple.namespace:
-                    continue
-                couples_to_rebucket.add(couple)
-        for net_key in net_keys:
-            for couple in self.couples_by_net.get(net_key):
-                if couple.namespace != couple_res.couple.namespace:
-                    continue
-                couples_to_rebucket.add(couple)
+
+        def populate_neighbours(keys, couples_by_key_container, neighbours):
+            for key in keys:
+                for couple in couples_by_key_container.get(key, []):
+                    if couple.namespace != couple_res.couple.namespace:
+                        continue
+                    if len(couple.groups) != couple_size:
+                        continue
+                    if couple in skip_couples:
+                        continue
+                    neighbours.add(couple)
+
+        populate_neighbours(disk_keys, self.couples_by_disk, couples_to_rebucket)
+        populate_neighbours(net_keys, self.couples_by_net, couples_to_rebucket)
 
         for couple in couples_to_rebucket:
             buckets.insert(self.couples[couple], updating=True)
@@ -169,6 +187,11 @@ class WeightManager(object):
         nets = [self.net[net_key] for net_key in couple_res.net_keys()]
         for resource in itertools.chain(disks, nets):
             resource.claim(resource_units)
+            logger.debug('Ns {}, claimed {} resource units from resource {}'.format(
+                couple_res.couple.namespace.id,
+                resource_units,
+                resource
+            ))
 
     @staticmethod
     def namespaces(storage):
@@ -181,15 +204,20 @@ class WeightManager(object):
 
 
 class WeightCalculator(object):
-    DISK_UTIL_COEF = config.get('weight', {}).get('resource_unit_disk_util', 0.05)
-    DISK_NET_RATE_COEF = config.get('weight', {}).get('resource_unit_net_write_rate',
-                                                      5 * (1024 ** 2))
+    DISK_UTIL_COEF = WEIGHT_CFG.get('resource_unit_disk_util', 0.05)
+    DISK_NET_RATE_COEF = WEIGHT_CFG.get('resource_unit_net_write_rate', 5 * (1024 ** 2))
+
+    ADD_RES_UNITS_REL = WEIGHT_CFG.get('add_resource_units_relative', 0.15)
+    ADD_RES_UNITS_ABS = WEIGHT_CFG.get('add_resource_units_absolute', 0.20)
 
     @staticmethod
     def ns_used_resources(ns):
         load = load_manager.namespaces[ns]
         max_coef = max(load.net_write_rate / WeightCalculator.DISK_NET_RATE_COEF,
                        load.disk_util_write / WeightCalculator.DISK_UTIL_COEF)
+        # add reserve resource units
+        max_coef = max(max_coef * (1.0 + WeightCalculator.ADD_RES_UNITS_REL),
+                       max_coef + WeightCalculator.ADD_RES_UNITS_ABS)
         return ResourceUnit(disk_util=max_coef * WeightCalculator.DISK_UTIL_COEF,
                             net_rate=max_coef * WeightCalculator.DISK_NET_RATE_COEF)
 
@@ -202,6 +230,63 @@ class WeightCalculator(object):
         return space_coef
 
     @staticmethod
+    def net_coef(x):
+        base_threshold_min_coef = 0.1
+        min_coef = 0.01
+        if x <= NetResources.WRITE_RATE_THRESHOLD * 0.5:
+            return 1.0
+        if x <= NetResources.WRITE_RATE_THRESHOLD:
+            return (
+                1.0 - (
+                   (1.0 - base_threshold_min_coef) *
+                   (x / (NetResources.WRITE_RATE_THRESHOLD * 0.5) - 1.0)
+                )
+            )
+        return max(
+            (
+                base_threshold_min_coef - (
+                    (base_threshold_min_coef - min_coef) *
+                    (
+                        (x - NetResources.WRITE_RATE_THRESHOLD) /
+                        (NetResources.MAX_WRITE_RATE - NetResources.WRITE_RATE_THRESHOLD)
+                    )
+                )
+            ),
+            min_coef
+        )
+
+    @staticmethod
+    def disk_coef(x):
+        def normal_mode_coef(x):
+            return math.e ** (-12 * (x ** 2))
+
+        if x <= DiskResources.DISK_UTIL_THRESHOLD:
+            return normal_mode_coef(x)
+        else:
+            base_point = normal_mode_coef(DiskResources.DISK_UTIL_THRESHOLD)
+            min_coef = 0.01
+            return max(
+                (
+                    base_point - (
+                        (base_point - min_coef) *
+                        (
+                            (x - DiskResources.DISK_UTIL_THRESHOLD) /
+                            (1.0 - DiskResources.DISK_UTIL_THRESHOLD)
+                        )
+                    )
+                ),
+                min_coef
+            )
+
+    @staticmethod
+    def weight(base_coef, net_coef, disk_coef):
+        return int(1000000 * base_coef * net_coef * disk_coef)
+
+    @staticmethod
+    def resource_coef(base_coef, net_coef, disk_coef):
+        return base_coef * net_coef * disk_coef
+
+    @staticmethod
     def calculate_resources(couple_res):
         """
         Get integer weight and resource units claimed from couple
@@ -209,22 +294,46 @@ class WeightCalculator(object):
         Returns:
             A tuple of (calculated couple weight, <ResourceUnit> that should be claimed)
         """
-        # TODO: get couple effective_free_space percentage
         couple = couple_res.couple
         used_space_pct = max(1.0 - float(couple.effective_free_space) / couple.effective_space,
                              0.0)
         base_coef = WeightCalculator.used_space_coef(used_space_pct)
-        logger.debug('Couple {} used_space_pct: {}, base weight" {}'.format(
-            couple_res.couple, used_space_pct, int(base_coef * 10000)))
+        net_coef = WeightCalculator.net_coef(couple_res.net_write_rate)
+        disk_coef = WeightCalculator.disk_coef(couple_res.disk_util)
+
+        weight = WeightCalculator.weight(base_coef, net_coef, disk_coef)
+
+        logger.info(
+            'Ns {}, couple {} used_space_pct: {}, base coef {}, '
+            'net_write_rate: {} Mb/s, net_coef {}, '
+            'disk_util: {}, disk_coef {}, '
+            'weight = {}'.format(
+                couple_res.couple.namespace.id,
+                couple_res.couple,
+                used_space_pct,
+                base_coef,
+                couple_res.net_write_rate,
+                net_coef,
+                couple_res.disk_util,
+                disk_coef,
+                weight
+            )
+        )
+
+        resource_coef = WeightCalculator.resource_coef(base_coef, net_coef, disk_coef)
         return (
-            int(base_coef * 10000),
-            ResourceUnit(disk_util=base_coef * WeightCalculator.DISK_UTIL_COEF,
-                         net_rate=base_coef * WeightCalculator.DISK_NET_RATE_COEF)
+            weight,
+            ResourceUnit(disk_util=resource_coef * WeightCalculator.DISK_UTIL_COEF,
+                         net_rate=resource_coef * WeightCalculator.DISK_NET_RATE_COEF)
         )
 
     @staticmethod
     def zero_resources():
         return ResourceUnit(disk_util=0.0, net_rate=0.0)
+
+    @staticmethod
+    def min_couple_resources():
+        return ResourceUnit(disk_util=0.0025, net_rate=float(250 * 1024))
 
 
 class CouplesBuckets(object):
@@ -240,8 +349,15 @@ class CouplesBuckets(object):
 
     @staticmethod
     def is_base(couple_res):
+        logger.debug('Couple {}: disk_util: {}, net_write_rate {}, groups_res {}, nbr_res {}'.format(
+            couple_res.couple,
+            couple_res.disk_util,
+            couple_res.net_write_rate,
+            len(couple_res.groups_res),
+            [nbr.net_write_rate for gr in couple_res.groups_res for nbr in gr.node_backends_res]
+        ))
         return (couple_res.disk_util < 0.7 and
-                couple_res.net_write_rate < 70 * (1024 ** 2) and
+                couple_res.net_write_rate < NetResources.WRITE_RATE_THRESHOLD and
                 couple_res.io_blocking_queue_size < 10 and
                 couple_res.io_nonblocking_queue_size < 10)
 
@@ -273,10 +389,16 @@ class CouplesBuckets(object):
         if couple_res.couple not in self.buckets_idx[bucket_id]:
             self.buckets[bucket_id].append(couple_res)
             self.buckets_idx[bucket_id].add(couple_res.couple)
+            logger.debug('Couple {} goes into bucket {} ({}){}'.format(
+                couple_res.couple,
+                bucket_id + 1,
+                self.BUCKET_ORDER[bucket_id],
+                ' (rebucketing)' if updating else ''
+            ))
             if updating:
                 for i, bucket in enumerate(self.buckets):
                     if i == bucket_id:
-                        continue
+                        break
                     if couple_res.couple in self.buckets_idx[i]:
                         self.skip.add(couple_res.couple)
                         self.buckets_idx[i].remove(couple_res.couple)
@@ -299,6 +421,10 @@ class ResourceUnit(object):
     def __add__(self, other):
         return ResourceUnit(disk_util=self.disk_util + other.disk_util,
                             net_rate=self.net_rate + other.net_rate)
+
+    def __sub__(self, other):
+        return ResourceUnit(disk_util=max(self.disk_util - other.disk_util, 0.0),
+                            net_rate=max(self.net_rate - other.net_rate, 0.0))
 
     def __iadd__(self, other):
         self.disk_util += other.disk_util
@@ -387,7 +513,11 @@ class NodeBackendResources(object):
 
 
 class DiskResources(object):
-    def __init__(self, disk_load):
+
+    DISK_UTIL_THRESHOLD = WEIGHT_CFG.get('disk', {}).get('disk_util_threshold', 0.4)
+
+    def __init__(self, key, disk_load):
+        self.key = key
 
         ext_write_rate = disk_load.write_rate - disk_load.ell_write_rate
         ext_write_rate_ratio = (
@@ -404,12 +534,20 @@ class DiskResources(object):
         return (nb.node.host.hostname, nb.fs.fsid)
 
     def claim(self, resource_units):
-        self.disk_util = max(self.disk_util + resource_units.disk_util,
+        self.disk_util = min(self.disk_util + resource_units.disk_util,
                              1.0)
+
+    def __repr__(self):
+        return '<Disk {}: disk_util: {:.4f}>'.format(self.key, self.disk_util)
 
 
 class NetResources(object):
-    def __init__(self, net_load):
+
+    WRITE_RATE_THRESHOLD = WEIGHT_CFG.get('net', {}).get('write_rate_threshold', 70 * (1024 ** 2))
+    MAX_WRITE_RATE = WEIGHT_CFG.get('net', {}).get('write_rate_threshold', 100 * (1024 ** 2))
+
+    def __init__(self, key, net_load):
+        self.key = key
         self.net_write_rate = net_load.write_rate - net_load.ell_write_rate
 
     @staticmethod
@@ -418,6 +556,12 @@ class NetResources(object):
 
     def claim(self, resource_units):
         self.net_write_rate = self.net_write_rate + resource_units.net_rate
+
+    def __repr__(self):
+        return '<Net {}: net_write_rate: {:.4f} Mb/s>'.format(
+            self.key,
+            self.net_write_rate / float(1024 ** 2)
+        )
 
 
 weight_manager = WeightManager()
