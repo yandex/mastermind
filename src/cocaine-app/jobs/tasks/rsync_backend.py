5cc7bef35d41ff1b224e07e8610a3e2e693b3434
Andrey Vasilenkov <indigo@yandex-team.ru>
2015-10-21 19:44:33 +0300

    Fix rsync backend task to be able to run even if all groups went down

--- src/cocaine-app/jobs/tasks/rsync_backend.py
+++ src/cocaine-app/jobs/tasks/rsync_backend.py
@@ -25,28 +25,35 @@ class RsyncBackendTask(MinionCmdTask):
         logger.info('Job {0}, task {1}: checking group {2} and node backend {3} '
             'state'.format(self.parent_job.id, self.id, self.group, self.node_backend))
 
-        if not self.group in storage.groups:
-            raise JobBrokenError('Group {0} is not found'.format(self.group))
-
-        group = storage.groups[self.group]
-
         if self.node_backend:
             # Check if old backend is down
             # This check is not applied to move job
+            current_group_node_backends = []
+            if self.group in storage.groups:
+                group = storage.groups[self.group]
+                current_group_node_backends = set([nb for nb in group.node_backends])
+
             if self.node_backend in storage.node_backends:
-                logger.info('Job {0}, task {1}: checking node backend status'.format(
-                    self.parent_job.id, self.id, self.node_backend))
-                node_backend = storage.node_backends[self.node_backend]
-                if (node_backend in group.node_backends and
-                    node_backend.status not in (storage.Status.STALLED, storage.Status.INIT, storage.Status.RO)):
+                old_node_backend = storage.node_backends[self.node_backend]
+                current_group_node_backends.discard(old_node_backend)
+                expected_statuses = (storage.Status.STALLED, storage.Status.INIT, storage.Status.RO)
+                if old_node_backend.status not in expected_statuses:
+                    raise JobBrokenError(
+                        'Node backend {nb} has status {status}, '
+                        'expected {expected_statuses}'.format(
+                            nb=old_node_backend,
+                            status=old_node_backend.status,
+                            expected_statuses=expected_statuses
+                        )
+                    )
 
-                    raise JobBrokenError('Node backend {0} has status, expected {1}'.format(
-                        node_backend.status, (storage.Status.STALLED, storage.Status.INIT, storage.Status.RO)))
-
-            elif len(group.node_backends) > 0:
-                raise JobBrokenError('Group {0} is running on backend {1} which '
-                    'does not match {2}'.format(self.group, str(group.node_backends[0]),
-                        self.node_backend))
+            if current_group_node_backends:
+                raise JobBrokenError(
+                    'Group {} is running on unexpected backends {}'.format(
+                        self.group,
+                        [str(nb) for nb in current_group_node_backends]
+                    )
+                )
 
         super(RsyncBackendTask, self).execute(processor)
 
