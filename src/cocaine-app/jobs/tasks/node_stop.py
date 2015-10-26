b6040c9f96744a9ddf1a37a562beb503c3d76eb4
Andrey Vasilenkov <indigo@yandex-team.ru>
2015-10-26 13:24:18 +0300

    Pepify

--- src/cocaine-app/jobs/tasks/node_stop.py
+++ src/cocaine-app/jobs/tasks/node_stop.py
@@ -20,34 +20,67 @@ class NodeStopTask(MinionCmdTask):
 
         if self.group:
             # checking if task still applicable
-            logger.info('Job {0}, task {1}: checking group {2} and host {3} '
-                'consistency'.format(self.parent_job.id, self.id, self.group, self.host))
+            logger.info(
+                'Job {job_id}, task {task_id}: checking group {group} and host {host} '
+                'consistency'.format(
+                    job_id=self.parent_job.id,
+                    task_id=self.id,
+                    group=self.group,
+                    host=self.host
+                )
+            )
 
-            if not self.group in storage.groups:
-                raise JobBrokenError('Task {0}: group {0} is not found'.format(self.id, self.group))
+            if self.group not in storage.groups:
+                raise JobBrokenError(
+                    'Task {task_id}: group {group} is not found'.format(
+                        task_id=self.id,
+                        group=self.group
+                    )
+                )
 
             group = storage.groups[self.group]
-            if (len(group.node_backends) > 1 or
-                (group.node_backends and group.node_backends[0].node.host.addr != self.host)):
 
-                raise JobBrokenError('Task {0}: group {1} has more than '
-                    'one node backend: {2}, expected one backend on host {3}'.format(self.id, self.group,
-                        [str(nb) for nb in group.node_backends], self.host))
+            nb_addresses = set(nb.node.host.addr for nb in group.node_backends)
+            nb_addresses.discard(self.host)
 
-            if (group.node_backends and
-                group.node_backends[0].status not in (storage.Status.OK, storage.Status.RO, storage.Status.STALLED)):
+            if nb_addresses:
+                raise JobBrokenError(
+                    'Task {task_id}: group {group} has unexpected node backends: '
+                    '{node_backends}, expected one backend on host {host}'.format(
+                        task_id=self.id,
+                        group=self.group,
+                        node_backends=list(nb_addresses),
+                        host=self.host
+                    )
+                )
 
-                raise JobBrokenError('Task {0}: node of group {1} has '
-                    'status {2}, should be OK, RO or STALLED'.format(self.id, self.group,
-                        group.node_backends[0].status))
+            valid_statuses = (storage.Status.OK, storage.Status.RO, storage.Status.STALLED)
+            if group.node_backends and group.node_backends[0].status not in valid_statuses:
+                raise JobBrokenError(
+                    'Task {task_id}: node of group {group} has '
+                    'status {status}, should be one of {valid_statuses}'.format(
+                        task_id=self.id,
+                        group=self.group,
+                        status=group.node_backends[0].status,
+                        valid_statuses=valid_statuses
+                    )
+                )
 
             if self.uncoupled:
                 if group.couple:
-                    raise JobBrokenError('Task {0}: group {1} happens to be '
-                        'already coupled'.format(self.id, self.group))
+                    raise JobBrokenError(
+                        'Task {task_id}: group {group} happens to be already coupled'.format(
+                            task_id=self.id,
+                            group=self.group
+                        )
+                    )
             else:
                 if not group.couple:
-                    raise JobBrokenError('Task {0}: group {1} is not '
-                        'coupled'.format(self.id, self.group))
+                    raise JobBrokenError(
+                        'Task {task_id}: group {group} is not coupled'.format(
+                            task_id=self.id,
+                            group=self.group
+                        )
+                    )
 
         super(NodeStopTask, self).execute(processor)
