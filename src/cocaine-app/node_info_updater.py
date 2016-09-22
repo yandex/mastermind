a92d0a8b25a696091e6ea5efbe6e6165291d91d3
Vladimir Nikulichev <saturnus@yandex-team.ru>
2016-09-22 23:40:29 +0300

    Class NodeInfoUpdaterBase

--- src/cocaine-app/node_info_updater.py
+++ src/cocaine-app/node_info_updater.py
@@ -25,6 +25,8 @@ import timed_queue
 import storage
 from weight_manager import weight_manager
 
+from node_info_updater_base import NodeInfoUpdaterBase
+
 
 logger = logging.getLogger('mm.balancer')
 
@@ -32,7 +34,7 @@ GROUPS_META_UPDATE_TASK_ID = 'groups_meta_update'
 COUPLES_META_UPDATE_TASK_ID = 'couples_meta_update'
 
 
-class NodeInfoUpdater(object):
+class NodeInfoUpdater(NodeInfoUpdaterBase):
     def __init__(self,
                  node,
                  job_finder,
@@ -41,6 +43,12 @@ class NodeInfoUpdater(object):
                  prepare_namespaces_states=False,
                  prepare_flow_stats=False,
                  statistics=None):
+        super(NodeInfoUpdater, self).__init__(node=node,
+                                              job_finder=job_finder,
+                                              couple_record_finder=couple_record_finder,
+                                              prepare_namespaces_states=prepare_namespaces_states,
+                                              prepare_flow_stats=prepare_flow_stats,
+                                              statistics=statistics)
         logger.info("Created NodeInfoUpdater")
         self.__node = node
         self.statistics = statistics
@@ -56,25 +64,13 @@ class NodeInfoUpdater(object):
         self.__session.cflags |= elliptics.command_flags.nolock
         self.__nodeUpdateTimestamps = (time.time(), time.time())
 
-        self.__cluster_update_lock = threading.Lock()
-
-        if prepare_namespaces_states and statistics is None:
-            raise AssertionError('Statistics is required for namespaces states calculation')
-        if prepare_flow_stats and statistics is None:
-            raise AssertionError('Statistics is required for flow stats calculation')
-        self._prepare_namespaces_states = prepare_namespaces_states
-        self._prepare_flow_stats = prepare_flow_stats
-
     def start(self):
         self._node_statistics_update()
         self._update_symm_groups()
 
-    def _start_tq(self):
-        self.__tq.start()
-
     def _node_statistics_update(self):
         try:
-            with self.__cluster_update_lock:
+            with self._cluster_update_lock:
 
                 start_ts = time.time()
                 logger.info('Cluster updating: node statistics collecting started')
@@ -93,7 +89,7 @@ class NodeInfoUpdater(object):
         finally:
             logger.info('Cluster updating: node statistics collecting finished, time: {0:.3f}'.format(time.time() - start_ts))
             reload_period = config.get('nodes_reload_period', 60)
-            self.__tq.add_task_in('_node_statistics_update', reload_period, self._node_statistics_update)
+            self._tq.add_task_in('_node_statistics_update', reload_period, self._node_statistics_update)
             self.__nodeUpdateTimestamps = self.__nodeUpdateTimestamps[1:] + (time.time(),)
 
     def _update_symm_groups(self):
@@ -119,18 +115,11 @@ class NodeInfoUpdater(object):
             logger.info('Cluster updating: updating group coupling info finished, time: {0:.3f}'.format(time.time() - start_ts))
             # TODO: change period
             reload_period = config.get('nodes_reload_period', 60)
-            self.__tq.add_task_in(GROUPS_META_UPDATE_TASK_ID, reload_period, self._update_symm_groups)
-
-    @h.concurrent_handler
-    def force_nodes_update(self, request):
-        logger.info('Forcing nodes update')
-        self._force_nodes_update()
-        logger.info('Cluster was successfully updated')
-        return True
+            self._tq.add_task_in(GROUPS_META_UPDATE_TASK_ID, reload_period, self._update_symm_groups)
 
     def _force_nodes_update(self, groups=None):
         try:
-            with self.__cluster_update_lock:
+            with self._cluster_update_lock:
                 self.update_status(groups=groups)
         except Exception as e:
             logger.info('Failed to update nodes status: {0}\n{1}'.format(e, traceback.format_exc()))
@@ -684,112 +673,3 @@ class NodeInfoUpdater(object):
                 )
             couple = group.couple
             couple.settings = cr.settings
-
-    @h.concurrent_handler
-    def force_update_namespaces_states(self, request):
-        start_ts = time.time()
-        logger.info('Namespaces states forced updating: started')
-        try:
-            namespaces_settings = self.namespaces_settings.fetch()
-            self._do_update_namespaces_states(namespaces_settings)
-        except Exception as e:
-            logger.exception('Namespaces states forced updating: failed')
-            self._namespaces_states.set_exception(e)
-        finally:
-            logger.info('Namespaces states forced updating: finished, time: {0:.3f}'.format(
-                time.time() - start_ts))
-
-    def _update_namespaces_states(self, namespaces_settings):
-        start_ts = time.time()
-        logger.info('Namespaces states updating: started')
-        try:
-            self._do_update_namespaces_states(namespaces_settings)
-        except Exception as e:
-            logger.exception('Namespaces states updating: failed')
-            self._namespaces_states.set_exception(e)
-        finally:
-            logger.info('Namespaces states updating: finished, time: {0:.3f}'.format(
-                time.time() - start_ts))
-
-    def _do_update_namespaces_states(self, namespaces_settings):
-        def default():
-            return {
-                'settings': {},
-                'couples': [],
-                'weights': {},
-                'statistics': {},
-            }
-
-        res = defaultdict(default)
-
-        # settings
-        for ns_settings in namespaces_settings:
-            res[ns_settings.namespace]['settings'] = ns_settings.dump()
-
-        # couples
-        for couple in storage.replicas_groupsets:
-            try:
-                try:
-                    ns = couple.namespace
-                except ValueError:
-                    continue
-                info = couple.info().serialize()
-                info['hosts'] = couple.groupset_hosts()
-                if couple.lrc822v1_groupset:
-                    info['groupsets'][storage.Group.TYPE_LRC_8_2_2_V1]['hosts'] = couple.lrc822v1_groupset.groupset_hosts()
-                # couples
-                res[ns.id]['couples'].append(info)
-            except Exception:
-                logger.exception(
-                    'Failed to include couple {couple} in namespace states'.format(
-                        couple=couple
-                    )
-                )
-                continue
-
-        # weights
-        for ns_id in weight_manager.weights:
-            res[ns_id]['weights'] = dict(
-                (str(k), v) for k, v in weight_manager.weights[ns_id].iteritems()
-            )
-            logger.info('Namespace {}: weights are updated by weight manager'.format(
-                ns_id
-            ))
-
-        # statistics
-        for ns, stats in self.statistics.per_ns_statistics().iteritems():
-            res[ns]['statistics'] = stats
-
-        # removing internal namespaces that clients should not know about
-        res.pop(storage.Group.CACHE_NAMESPACE, None)
-
-        self._namespaces_states.set_result(dict(res))
-
-    @h.concurrent_handler
-    def force_update_flow_stats(self, request):
-        start_ts = time.time()
-        logger.info('Flow stats forced updating: started')
-        try:
-            self._do_update_flow_stats()
-        finally:
-            logger.info('Flow stats forced updating: finished, time: {0:.3f}'.format(
-                time.time() - start_ts))
-
-    def _update_flow_stats(self):
-        start_ts = time.time()
-        logger.info('Flow stats updating: started')
-        try:
-            self._do_update_flow_stats()
-        finally:
-            logger.info('Flow stats updating: finished, time: {0:.3f}'.format(
-                time.time() - start_ts))
-
-    def _do_update_flow_stats(self):
-        try:
-            self._flow_stats = self.statistics.calculate_flow_stats()
-        except Exception as e:
-            logger.exception('Flow stats updating: failed')
-            self._flow_stats = e
-
-    def stop(self):
-        self.__tq.shutdown()
---
+++ src/cocaine-app/node_info_updater_base.py
@@ -0,0 +1,214 @@
+# -*- coding: utf-8 -*-
+from collections import defaultdict
+import logging
+import re
+import threading
+import time
+import traceback
+
+import elliptics
+
+# import balancer
+from config import config
+import helpers as h
+from infrastructure import infrastructure
+from jobs import Job
+import keys
+from load_manager import load_manager
+from mastermind import helpers as mh
+from mastermind.pool import skip_exceptions
+from mastermind import MastermindClient
+from mastermind_core.response import CachedGzipResponse
+from mastermind_core import errors
+from monitor_pool import monitor_pool
+import timed_queue
+import storage
+from weight_manager import weight_manager
+
+logger = logging.getLogger('mm.balancer')
+
+
+class NodeInfoUpdaterBase(object):
+    def __init__(self,
+                 node,
+                 job_finder,
+                 couple_record_finder,
+                 prepare_namespaces_states,
+                 prepare_flow_stats,
+                 statistics):
+        self.abcdef = None
+        self._node = node
+        self.statistics = statistics
+        self.job_finder = job_finder
+        self.couple_record_finder = couple_record_finder
+        self._namespaces_states = CachedGzipResponse()
+        self._flow_stats = {}
+        self._tq = timed_queue.TimedQueue()
+        self._cluster_update_lock = threading.Lock()
+
+        if prepare_namespaces_states and statistics is None:
+            raise AssertionError('Statistics is required for namespaces states calculation')
+        if prepare_flow_stats and statistics is None:
+            raise AssertionError('Statistics is required for flow stats calculation')
+        self._prepare_namespaces_states = prepare_namespaces_states
+        self._prepare_flow_stats = prepare_flow_stats
+
+    def _start_tq(self):
+        self._tq.start()
+
+    def _update_max_group(self):
+        def _do_update_max_group(response, elapsed_time=None, end_time=None):
+            if response.error.code:
+                if response.error.code == error.ELLIPTICS_NOT_FOUND:
+                    max_group = 0
+                else:
+                    raise RuntimeError(respose.error.message)
+            else:
+                max_group = int(response.data)
+
+            cur_max_group = max((g.group_id for g in storage.groups))
+            logger.info('Current max group in storage: {}'.format(cur_max_group))
+
+            if cur_max_group > max_group:
+                logger.info('Updating storage max group to {}'.format(max_group))
+                self._node.meta_session.write_data(
+                    keys.MASTERMIND_MAX_GROUP_KEY,
+                    str(cur_max_group)).get()
+
+        if not len(storage.groups):
+            logger.warn('No groups found in storage, not updating max group')
+            return
+
+        try:
+            session = self._node.meta_session.clone()
+            session.set_exceptions_policy(elliptics.exceptions_policy.no_exceptions)
+            result = session.read_data(keys.MASTERMIND_MAX_GROUP_KEY)
+
+            try:
+                h.process_elliptics_async_result(
+                    result,
+                    _do_update_max_group,
+                    raise_on_error=False)
+            except Exception as e:
+                log.error('Failed to update max group number: {}'.format(e))
+        except Exception as e:
+            logger.error('Failed to read max group: {}'.format(e))
+
+    @h.concurrent_handler
+    def force_nodes_update(self, request):
+        logger.info('Forcing nodes update')
+        self._force_nodes_update()
+        logger.info('Cluster was successfully updated')
+        return True
+
+    def _force_nodes_update(self, groups=None):
+        raise NotImplemented('_force_nodes_update() must be implemented in derived class')
+
+    @h.concurrent_handler
+    def force_update_namespaces_states(self, request):
+        start_ts = time.time()
+        logger.info('Namespaces states forced updating: started')
+        try:
+            self._do_update_namespaces_states()
+        except Exception as e:
+            logger.exception('Namespaces states forced updating: failed')
+            self._namespaces_states.set_exception(e)
+        finally:
+            logger.info('Namespaces states forced updating: finished, time: {0:.3f}'.format(
+                time.time() - start_ts))
+
+    def _update_namespaces_states(self):
+        start_ts = time.time()
+        logger.info('Namespaces states updating: started')
+        try:
+            self._do_update_namespaces_states()
+        except Exception as e:
+            logger.exception('Namespaces states updating: failed')
+            self._namespaces_states.set_exception(e)
+        finally:
+            logger.info('Namespaces states updating: finished, time: {0:.3f}'.format(
+                time.time() - start_ts))
+
+    def _do_update_namespaces_states(self):
+        def default():
+            return {
+                'settings': {},
+                'couples': [],
+                'weights': {},
+                'statistics': {},
+            }
+
+        res = defaultdict(default)
+
+        # settings
+        ns_settings = infrastructure.ns_settings
+        for ns, settings in ns_settings.items():
+            res[ns]['settings'] = settings
+
+        # couples
+        for couple in storage.replicas_groupsets:
+            try:
+                try:
+                    ns = couple.namespace
+                except ValueError:
+                    continue
+                info = couple.info().serialize()
+                info['hosts'] = couple.groupset_hosts()
+                if couple.lrc822v1_groupset:
+                    info['groupsets'][storage.Group.TYPE_LRC_8_2_2_V1]['hosts'] = couple.lrc822v1_groupset.groupset_hosts()
+                # couples
+                res[ns.id]['couples'].append(info)
+            except Exception:
+                logger.exception(
+                    'Failed to include couple {couple} in namespace states'.format(
+                        couple=couple
+                    )
+                )
+                continue
+
+        # weights
+        for ns_id in weight_manager.weights:
+            res[ns_id]['weights'] = dict(
+                (str(k), v) for k, v in weight_manager.weights[ns_id].iteritems()
+            )
+            logger.info('Namespace {}: weights are updated by weight manager'.format(
+                ns_id
+            ))
+
+        # statistics
+        for ns, stats in self.statistics.per_ns_statistics().iteritems():
+            res[ns]['statistics'] = stats
+
+        # removing internal namespaces that clients should not know about
+        res.pop(storage.Group.CACHE_NAMESPACE, None)
+
+        self._namespaces_states.set_result(dict(res))
+
+    @h.concurrent_handler
+    def force_update_flow_stats(self, request):
+        start_ts = time.time()
+        logger.info('Flow stats forced updating: started')
+        try:
+            self._do_update_flow_stats()
+        finally:
+            logger.info('Flow stats forced updating: finished, time: {0:.3f}'.format(
+                time.time() - start_ts))
+
+    def _update_flow_stats(self):
+        start_ts = time.time()
+        logger.info('Flow stats updating: started')
+        try:
+            self._do_update_flow_stats()
+        finally:
+            logger.info('Flow stats updating: finished, time: {0:.3f}'.format(
+                time.time() - start_ts))
+
+    def _do_update_flow_stats(self):
+        try:
+            self._flow_stats = self.statistics.calculate_flow_stats()
+        except Exception as e:
+            logger.exception('Flow stats updating: failed')
+            self._flow_stats = e
+
+    def stop(self):
+        self._tq.shutdown()
