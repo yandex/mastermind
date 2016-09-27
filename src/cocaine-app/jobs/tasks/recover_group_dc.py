8ace02abc9475f0bf87ddef1464029cef618b2cc
Andrey Vasilenkov <indigo@yandex-team.ru>
2016-09-27 15:04:39 +0300

    Skip -2 and -77 statuses when parsing recover dc command results

--- src/cocaine-app/jobs/tasks/recover_group_dc.py
+++ src/cocaine-app/jobs/tasks/recover_group_dc.py
@@ -49,3 +49,53 @@ class RecoverGroupDcTask(MinionCmdTask):
                                        group.couple, self.couple))
 
         super(RecoverGroupDcTask, self).execute(processor)
+
+    STATUSES_TO_SKIP = (
+        -2,   # No such file or directory (key is not found)
+        -77,  # File descriptor in bad state (concurrent key write error when CAS is used)
+    )
+
+    def failed(self, processor):
+        if self.minion_cmd is None:
+            return True
+
+        success = (
+            self.minion_cmd['exit_code'] == 0 or
+            self.minion_cmd.get('command_code') in self.params.get('success_codes', [])
+        )
+        if success:
+            return False
+
+        commands_statuses = self.minion_cmd.get('commands_statuses')
+        if commands_statuses:
+            logger.info(
+                'Job {job_id}, task {task_id}: checking recovery commands statuses: '
+                '{commands_statuses}'.format(
+                    job_id=self.parent_job.id,
+                    task_id=self.id,
+                    commands_statuses=commands_statuses,
+                )
+            )
+            statuses = {
+                int(status): count
+                for command, status_count in commands_statuses.iteritems()
+                for status, count in status_count.iteritems()
+            }
+
+            for status, count in statuses.iteritems():
+                if status not in self.STATUSES_TO_SKIP and count > 0:
+                    logger.error(
+                        'Job {job_id}, task {task_id}: commands statuses contain status that '
+                        'cannot be skipped: {status} (count {count})'.format(
+                            job_id=self.parent_job.id,
+                            task_id=self.id,
+                            status=status,
+                            count=count,
+                        )
+                    )
+                    return True
+
+            return False
+
+        return True
+
