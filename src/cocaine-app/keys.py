561f305c25be00bb490f03c174f7ad9a38fb119b
Andrey Vasilenkov <indigo@yandex-team.ru>
2016-10-14 14:36:52 +0300

    Store inventory cache on a file system instead of metaelliptics

--- src/cocaine-app/keys.py
+++ src/cocaine-app/keys.py
@@ -2,19 +2,3 @@
 SYMMETRIC_GROUPS_KEY = 'metabalancer\0symmetric_groups'
 
 MASTERMIND_MAX_GROUP_KEY = 'mastermind:max_group'
-MASTERMIND_COUPLE_META_KEY = 'mastermind:couple_meta:%s'
-
-MM_GROUPS_IDX = 'mastermind:groups_idx'
-MM_ISTRUCT_GROUP = 'mastermind:group_%d'
-
-MM_DC_CACHE_IDX = 'mastermind:dc_cache'
-MM_DC_CACHE_HOST = 'mastermind:dc_cache_%s'
-
-MM_HOSTNAME_CACHE_IDX = 'mastermind:hostname_cache'
-MM_HOSTNAME_CACHE_HOST = 'mastermind:hostname_cache_%s'
-
-MM_HOSTTREE_CACHE_IDX = 'mastermind:hosttree_cache'
-MM_HOSTTREE_CACHE_HOST = 'mastermind:hosttree_cache_%s'
-
-MM_IPADDRESSES_CACHE_IDX = 'mastermind:ipaddresses_cache'
-MM_IPADDRESSES_CACHE_HOST = 'mastermind:ipaddresses_cache_%s'
