1205a533b8791826b71dae672812fc1825b9bf5a
Andrey Vasilenkov <indigo@yandex-team.ru>
2016-10-17 13:07:37 +0300

    Setup cocaine worker logger

--- src/cocaine-app/log.py
+++ src/cocaine-app/log.py
@@ -19,12 +19,17 @@ def setup_logger(logger_name='logging'):
     tornado_logger = logging.getLogger('tornado')
     tornado_logger.propagate = False
 
+    worker_logger = logging.getLogger('cocaine.worker')
+    worker_logger.propagate = False
+
     _handler = FileHandler('/var/log/cocaine-core/file.log')
     _handler.setFormatter(logging.Formatter(fmt='[%(asctime)s] [%(levelname)s] [%(name)s] [%(process)d] %(message)s'))
 
     root_app_logger.addHandler(_handler)
     tornado_logger.addHandler(_handler)
+    worker_logger.addHandler(_handler)
 
     # cocaine.Logger will take care of low-level messages filtering
     root_app_logger.setLevel(logging.DEBUG)
     tornado_logger.setLevel(logging.DEBUG)
+    worker_logger.setLevel(logging.DEBUG)
